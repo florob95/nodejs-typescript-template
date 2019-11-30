cd173a77fedc08b4fce7db0a153dc2edfea5d768
florob <florian.robert@skillogs.com>
2019-11-30 19:30:13 +0100

    add mocha and chai for tu

---
+++ src/test/service/ExampleServiceTest.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import 'mocha';
+import { Container } from 'typedi';
+import { ExampleService } from '../../services/ExampleService';
+
+describe('ExamplesService Test', () => {
+  it('Find All return array', () => {
+    expect(Container.get(ExampleService).findAll()).to.be.an('array');
+  });
+});
